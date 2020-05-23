7e2378cf4de4a0583d5c467ddb9218fe61d65c87
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-23 21:37:06 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog 
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name =(name)
+    @name = name 
+  end 
+  
+  def breed= (breed)
+    @breed = breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end 
+
+name= Dog.new("Mutt")
+name.breed 
+    
